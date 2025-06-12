9796692eb59b71c08908614c833f99bb6d3ae7a2
Samuel Lando <samuel.lando@aol.com>
2025-06-11 21:34:01 -0400

    Add ability to remove items

--- frontends/nvim/lua/fedilist/init.lua
+++ frontends/nvim/lua/fedilist/init.lua
@@ -78,7 +78,7 @@ function M.display(url)
 
     -- Prepare lines
     local lines = {
-        "Name: " .. data.name,
+        data.name,
         "--------------------------------"
     }
     local ids = {}
@@ -103,45 +103,92 @@ end
 
 function M.on_save(buf)
     local ids = vim.b[buf].ids or {}
+    local matched = {}
     local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
     for _, line in ipairs(lines) do
         local rid = line:match("^\\(%w%w%w)%s")
         local name = line:match("^%- (.+)")
-        if not (rid and ids[rid]) and name then
-            local payload = {
-                ["@context"] = {
-                    "https://schema.org",
-                    {
-                        owner   = "http://fedilist.com/owner",
-                        editor  = "http://fedilist.com/editor",
-                        viewer  = "http://fedilist.com/viewer",
-                        atIndex = "http://fedilist.com/toIndex",
-                        Result  = "http://fedilist.com/Result"
-                    }
-                },
-                type = "AppendAction",
-                agent = {
-                    type = "Person",
-                    id   = "http://localhost:9090/user/samuel"
-                },
-                object = {
-                    type = "ItemList",
-                    name = name
-                },
-                targetCollection = {
-                    type = "ItemList",
-                    id   = vim.b[buf].id
-                },
-                startTime = iso_datetime()
-            }
-            local body = vim.fn.json_encode(payload)
-            local cmd = 'curl -s -X POST -H "Content-Type: application/json" --data ' ..
-                vim.fn.shellescape(body) .. ' http://localhost:9090/user/samuel/outbox'
-            os.execute(cmd)
+        if not rid then
+            if not (rid and ids[rid]) and name then
+                M.append(buf, name)
+            end
+        else
+            matched[rid] = true
+        end
+    end
+    for rid, _ in pairs(ids) do
+        if not matched[rid] then
+            M.remove(buf, ids[rid])
         end
     end
     vim.api.nvim_buf_set_option(buf, "modified", false)
     M.display(vim.b[buf].id)
+end
+
+function M.remove(buf, id)
+    local payload = {
+        ["@context"] = {
+            "https://schema.org",
+            {
+                owner   = "http://fedilist.com/owner",
+                editor  = "http://fedilist.com/editor",
+                viewer  = "http://fedilist.com/viewer",
+                atIndex = "http://fedilist.com/toIndex",
+                Result  = "http://fedilist.com/Result"
+            }
+        },
+        type = "RemoveAction",
+        agent = {
+            type = "Person",
+            id   = "http://localhost:9090/user/samuel"
+        },
+        object = {
+            type = "ItemList",
+            id = id
+        },
+        targetCollection = {
+            type = "ItemList",
+            id   = vim.b[buf].id
+        },
+        startTime = iso_datetime()
+    }
+    local body = vim.fn.json_encode(payload)
+    local cmd = 'curl -s -X POST -H "Content-Type: application/json" --data ' ..
+        vim.fn.shellescape(body) .. ' http://localhost:9090/user/samuel/outbox'
+    os.execute(cmd)
+end
+
+function M.append(buf, name)
+    local payload = {
+        ["@context"] = {
+            "https://schema.org",
+            {
+                owner   = "http://fedilist.com/owner",
+                editor  = "http://fedilist.com/editor",
+                viewer  = "http://fedilist.com/viewer",
+                atIndex = "http://fedilist.com/toIndex",
+                Result  = "http://fedilist.com/Result"
+            }
+        },
+        type = "AppendAction",
+        agent = {
+            type = "Person",
+            id   = "http://localhost:9090/user/samuel"
+        },
+        object = {
+            type = "ItemList",
+            name = name
+        },
+        targetCollection = {
+            type = "ItemList",
+            id   = vim.b[buf].id
+        },
+        startTime = iso_datetime()
+    }
+    local body = vim.fn.json_encode(payload)
+    local cmd = 'curl -s -X POST -H "Content-Type: application/json" --data ' ..
+        vim.fn.shellescape(body) .. ' http://localhost:9090/user/samuel/outbox'
+    os.execute(cmd)
 end
 
 function M.on_enter(buf)
